cdfbf8401c0ac0f1067b2908efa6f144aea7294c
Stacy <jlhclientsolutions@gmail.com>
2024-09-14 13:23:24 -0400

    restarting module challenge, added starter code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -129,7 +129,7 @@ const handleRenderBtns = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
-  let jsonNotes = await notes.JSON();
+  let jsonNotes = await notes.json();
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
