c911d9c8ee11266afa9c9aa9177ca623fe1a7c64
Stacy <jlhclientsolutions@gmail.com>
2024-09-14 19:17:14 -0400

    added port from render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
